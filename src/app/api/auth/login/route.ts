df2cadc8e77b317705bd0fee16c6924758eaff80
m-kos.th <m@kos.co.th>
2025-04-24 03:34:42 +0900

    fix: remove no-use

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -3,7 +3,7 @@
 import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
-  const { email, providerUUID, provider } = await req.json();
+  const { email } = await req.json();
 
   // Check if the user exists in the database
   const user = await prisma.user.findUnique({
